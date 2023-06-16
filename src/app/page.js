ee9de6c74eb73c7c36de95c65a5732e0c3d34e5c
Alex <alex@q-83.com>
2023-06-16 21:20:31 +1000

    feature/education: update page bg color

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Education from "../components/Education";
 
 export default function Home() {
   return (
-    <div className="w-full lg:w-4/5 bg-blue-200 sm:w-full m-auto">
+    <div className="w-full lg:w-4/5 bg-white sm:w-full m-auto">
       <Education/>
     </div>
   );

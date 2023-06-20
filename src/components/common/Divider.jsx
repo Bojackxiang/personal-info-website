372a380862d21b27c8172ce57a8f369695153034
Alex <alex@q-83.com>
2023-06-20 23:07:51 +1000

    feature/experience: update style

--- src/components/common/Divider.jsx
+++ src/components/common/Divider.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {twMerge} from 'tailwind-merge'
 
-const Divider = ({ children }) => {
+const Divider = ({ children, className }) => {
   return (
-    <div className="flex items-center my-5">
+    <div className={twMerge("flex items-center my-5", className)}>
       <div className="border-b-2 border-neutral-100 w-full" />
       <span className="px-2 py-0.5 font-medium text-neutral-700 text-3xl italic">
         {children}
--- src/components/experience/WorkExperience.jsx
+++ src/components/experience/WorkExperience.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import ExperienceText from "./ExperienceText";
 import ExperienceImage from "./ExperienceImage";
